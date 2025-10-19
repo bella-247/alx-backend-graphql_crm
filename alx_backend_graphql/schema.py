6c275a582c18018b44b8ab4c758e9a2705746aaa
abelmekonen <abelmekonen247@gmail.com>
2025-10-19 20:36:43 +0300

    Remove unused hello field from Query class in schema.py

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -2,6 +2,6 @@ import graphene
 from crm.schema import CRMQuery
 
 class Query(CRMQuery, graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
-    
+    pass
+
 schema = graphene.Schema(query=Query)
