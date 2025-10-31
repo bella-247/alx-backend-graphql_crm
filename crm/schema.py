4b36126a7de75ac5485ad0023e094570069c8ee2
abelmekonen <abelmekonen247@gmail.com>
2025-10-31 16:28:39 +0300

    updated typo

--- crm/schema.py
+++ crm/schema.py
@@ -5,7 +5,7 @@ from graphene_django.filter import DjangoFilterConnectionField
 from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.utils import timezone
-from crm.models import Customer, Product, Order
+from crm.models import Product, Customer, Order
 from crm.filters import CustomerFilter, ProductFilter, OrderFilter
 
 # Types
