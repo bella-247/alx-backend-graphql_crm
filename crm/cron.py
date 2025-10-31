02ff926833da15e172d9616309ab4b0a8e59ed59
abelmekonen <abelmekonen247@gmail.com>
2025-10-31 15:54:04 +0300

    Add CRM heartbeat logging and Django settings configuration

--- crm/cron.py
+++ crm/cron.py
@@ -1,9 +1,29 @@
 import logging
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
 
 log_file = "/tmp/crm_heartbeat_log.txt"
 logging.basicConfig(
     filename=log_file, level=logging.INFO, format="%(asctime)s - %(message)s"
 )
 
+transport = RequestsHTTPTransport(
+    url="http://localhost:8000/graphql/", retries=1, verify=False
+)
+
+client = Client(transport=transport, fetch_schema_from_transport=False)
+
+
+query = gql(
+    """ 
+{
+    hello
+}            
+"""
+)
+
+
 def log_crm_heartbeat():
+    client.execute(query)
     logging.info("CRM is alive")
